d7db74c7d0e7502bae3b2fb9172fee9dc6b918c3
schematics <dmitrijkrivoguzov@gmail.com>
2025-03-03 11:58:06 +0700

    added event hometask

--- Assets/Scripts/Events/Display.cs
+++ Assets/Scripts/Events/Display.cs
@@ -1,6 +1,26 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Display : MonoBehaviour
 {
+    [SerializeField] private WeatherStation _weatherStation;
+    [SerializeField] private Text _text;
     
+    void OnEnable()
+    {
+        _weatherStation.TemperatureChanged += GetTemperature;
+    }
+
+    private void OnDisable()
+    {
+        _weatherStation.TemperatureChanged -= GetTemperature;
+    }
+
+    // ReSharper disable Unity.PerformanceAnalysis
+    private void GetTemperature()
+    {
+        _text.text = $"Current temperature: {Random.Range(20, 30)}\u00b0C";
+        Debug.Log($"Current temperature: {Random.Range(20, 30)}\u00b0C");
+    }
 }
--- Assets/Scripts/Events/WeatherStation.cs
+++ Assets/Scripts/Events/WeatherStation.cs
@@ -1,6 +1,28 @@
+using System;
+using System.Collections;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class WeatherStation : MonoBehaviour
 {
-    
+    public event Action TemperatureChanged;
+
+    [SerializeField] private float _delay = 5f;
+
+    void Start()
+    {
+        StartCoroutine(GetTemperature(_delay));
+    }
+
+    IEnumerator GetTemperature(float delay)
+    {
+        WaitForSeconds wait = new WaitForSeconds(delay);
+
+        while (enabled)
+        {
+            TemperatureChanged?.Invoke();
+            
+            yield return wait;
+        }
+    }
 }
